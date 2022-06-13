aaaf20b8ade1b386a54dd4f808e55ae5606b53c1
Sunflwr15 <nabil.gathfan@gmail.com>
2022-06-13 12:35:00 +0700

    ghtfnUAS

--- valid.js
+++ valid.js
@@ -1,41 +1,27 @@
-	//Validtion Code For Inputs
+function btn() {
+	var input = document.getElementById("input").value;
+	var subs = ["Admin"];
 
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
-
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
-
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
-
-function validated(){
-	if (email.value.length < 9) {
-		email.style.border = "1px solid red";
-		email_error.style.display = "block";
-		email.focus();
-		return false;
+	for (let i = 0; i < subs.length; i++) {
+	  if (input.includes(subs[i])) {
+		location.replace("home.html");
+	  } else {
+		alert("Name Incorrect");
+		location.replace("index.html");
+	  }
 	}
-	if (password.value.length < 6) {
-		password.style.border = "1px solid red";
-		pass_error.style.display = "block";
-		password.focus();
-		return false;
-	}
+  }
 
+function btn_pass() {
+	var input_pass = document.getElementById("input_pass").value;
+	var subs_pass = ["Root"];
+
+	for (let i = 0; i < subs_pass.length; i++) {
+		if (input_pass.includes(subs_pass[i])) {
+		  location.replace("home.html");
+		} else {
+		alert("Password Incorrect");
+		  location.replace("index.html")
+		}
+	  }
 }
-function email_Verify(){
-	if (email.value.length >= 8) {
-		email.style.border = "1px solid silver";
-		email_error.style.display = "none";
-		return true;
-	}
-}
-function pass_Verify(){
-	if (password.value.length >= 5) {
-		password.style.border = "1px solid silver";
-		pass_error.style.display = "none";
-		return true;
-	}
-}
-
